0ac193397bb7fd17be3e08eea8357e1a3afa6ed4
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-25 16:50:35 +0300

    Drop shelljs dependency

--- support/build_doc.mjs
+++ support/build_doc.mjs
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
-import shell from 'shelljs'
+import { rmSync } from 'node:fs'
+import { execFileSync, spawnSync } from 'node:child_process'
 
-shell.rm('-rf', 'apidoc')
+rmSync('apidoc', { recursive: true, force: true })
 
-const head = shell.exec('git show-ref --hash HEAD').stdout.slice(0, 6)
+const head = execFileSync('git', ['show-ref', '--hash', 'HEAD'], { encoding: 'utf8' }).slice(0, 6)
 
 const link_format = `https://github.com/{package.repository}/blob/${head}/{file}#L{line}`
 
-shell.exec(`node node_modules/.bin/ndoc --alias mjs:js --link-format "${link_format}"`)
+const result = spawnSync(process.execPath, [
+  'node_modules/.bin/ndoc',
+  '--alias',
+  'mjs:js',
+  '--link-format',
+  link_format
+], { stdio: 'inherit' })
+
+if (result.error) throw result.error
+
+process.exitCode = result.status
