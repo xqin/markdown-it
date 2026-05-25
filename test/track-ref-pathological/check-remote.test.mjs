8e9f7448e693a27ffe044ae02a47267a6a4fcd37
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-25 08:05:30 +0300

    Reduce dev deps

--- test/track-ref-pathological/check-remote.test.mjs
+++ test/track-ref-pathological/check-remote.test.mjs
@@ -1,4 +1,3 @@
-import needle from 'needle'
 import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import crypto from 'node:crypto'
@@ -7,8 +6,11 @@ import { readFileSync } from 'fs'
 describe('Verify original reference', () => {
   it('verify original source crc', async () => {
     /* eslint-disable  max-len */
-    const src = await needle('get', 'https://raw.githubusercontent.com/commonmark/cmark/master/test/pathological_tests.py')
-    const src_md5 = crypto.createHash('md5').update(src.body).digest('hex')
+    const response = await fetch('https://raw.githubusercontent.com/commonmark/cmark/master/test/pathological_tests.py')
+
+    assert.strictEqual(response.ok, true, `Unable to fetch cmark pathological tests: ${response.status} ${response.statusText}`)
+
+    const src_md5 = crypto.createHash('md5').update(await response.text()).digest('hex')
     const tracked_md5 = JSON.parse(readFileSync(new URL('./pathological.json', import.meta.url))).md5
 
     assert.strictEqual(
