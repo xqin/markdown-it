6ff115a9072d3af8abe4e4447c09199fec1195d2
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-05-25 14:49:44 +0300

    rollup => vite

---
+++ support/build-dist.mjs
@@ -0,0 +1,96 @@
+import { rm } from 'node:fs/promises'
+import { createRequire } from 'node:module'
+import { build } from 'vite'
+
+const require = createRequire(import.meta.url)
+const pkg = require('../package.json')
+
+const banner = `/*! ${pkg.name} ${pkg.version} https://github.com/${pkg.repository} @license ${pkg.license} */`
+const external = Object.keys(pkg.dependencies ?? {})
+
+const common = {
+  configFile: false,
+  logLevel: 'info',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: false,
+    sourcemap: false,
+    target: 'es2015'
+  }
+}
+
+await rm('dist', { recursive: true, force: true })
+
+await build({
+  ...common,
+  build: {
+    ...common.build,
+    minify: 'terser',
+    terserOptions: {
+      mangle: false,
+      compress: false,
+      format: {
+        ascii_only: true,
+        beautify: true,
+        comments: 'all',
+        indent_level: 2
+      }
+    },
+    lib: {
+      entry: 'index.mjs',
+      name: 'markdownit',
+      formats: ['umd'],
+      fileName: () => 'markdown-it.js'
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        banner
+      }
+    }
+  }
+})
+
+await build({
+  ...common,
+  build: {
+    ...common.build,
+    minify: 'terser',
+    terserOptions: {
+      format: {
+        ascii_only: true
+      }
+    },
+    lib: {
+      entry: 'index.mjs',
+      name: 'markdownit',
+      formats: ['umd'],
+      fileName: () => 'markdown-it.min.js'
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        banner
+      }
+    }
+  }
+})
+
+await build({
+  ...common,
+  build: {
+    ...common.build,
+    minify: false,
+    lib: {
+      entry: 'index.mjs',
+      formats: ['cjs'],
+      fileName: () => 'index.cjs.js'
+    },
+    rollupOptions: {
+      external,
+      output: {
+        exports: 'default'
+      }
+    }
+  }
+})
